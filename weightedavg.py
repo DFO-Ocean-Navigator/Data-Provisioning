ab0c3f5be93e4286ff93b61c7b30059282ac775a
jamesprayner <jamesprayner@gmail.com>
2020-03-16 09:46:56 -0230

    WIP stuff for teleworking

--- weightedavg.py
+++ weightedavg.py
@@ -4,6 +4,7 @@ import numpy
 import xarray
 import sys
 
+import re
 # Required Arguments:
 #  finalHour    int     Last file to be processed 
 #  Run          str     Fully Qualified path up until run hour
@@ -34,18 +35,38 @@ fileNames = []
 for i in range(finalHour - 3, finalHour + 1):
     fileNames.append(f"{run}_00{i}_{dim}_ps5km60N.nc")
 
-dataFiles = xarray.open_mfdataset(fileNames)
+
+anchor = xarray.open_dataset(fileNames[0])
+print(anchor['polar_stereographic'].values)
+
+
+
+for i in range(1,4):
+    anchor = anchor.merge(xarray.open_dataset(fileNames[i]))
+print(anchor)
+print(anchor['polar_stereographic'].values)
+singleTest = xarray.open_dataset(fileNames[0])
+
+dataFiles = xarray.open_mfdataset(fileNames, data_vars='minimal')
+timeAttrs = dataFiles['time'].attrs
 finalTime = dataFiles['time'].values[-1]
 
-newData = dataFiles.isel({'time': 0})
 
-count = 0
+newData = dataFiles.isel({'time': 0})
+print(newData)
 
 for var in dataFiles.variables:
-    if count > 5:
+    if re.match(r"xc|yc|lat|lon|depth|time|pol", var) is None:
+        print(var)
+        #if var not in "yc xc latitude longitude depth time polar_stereographic":
+        oldAttrs = dataFiles[var].attrs
         avgd = numpy.average(dataFiles[var], 0, weights=weights)
         newData[var] = xarray.DataArray(avgd, [dataFiles['yc'], dataFiles['xc']])
-    count += 1
+        newData[var].attrs = oldAttrs
+
 
 newData = newData.expand_dims({'time': finalTime})
+
+newData['time'].attrs = timeAttrs
+print(newData)
 newData.to_netcdf("output.nc")
