5e63272eeeb773d461c5f64d1cad215a21432891
jamesprayner <jamesprayner@gmail.com>
2020-03-19 14:20:50 -0230

    Array changes and timestamps

--- riopsMonthly.sh
+++ riopsMonthly.sh
@@ -2,13 +2,14 @@
 # -------------------------
 # Generate monthly averages
 # -------------------------
+logfile='riopsMonthly.log'
 
 # 3h average directory
-riopsDir=''
-outDir=''
-days="31 28 31 30 31 30 31 31 30 31 30 31"
-daysleap="31 29 31 30 31 30 31 31 30 31 30 31"
-
+riopsDir='/home/buildadm/linkout'
+outDir='/home/buildadm/monthlyout'
+dims="2D 3D"
+days=(31 28 31 30 31 30 31 31 30 31 30 31)
+daysleap=(31 29 31 30 31 30 31 31 30 31 30 31)
 
 if [ ! -d ${outDir} ]
 then
@@ -22,7 +23,7 @@ for dim in $dims ; do
         mkdir ${outDir}/${dim}/
     fi
 
-    allFiles=`find ${outdir} -type f -name "*${dim}_ps5km60N.nc"`
+    allFiles=`find ${riopsDir} -type l,f -name "*${dim}_ps5km60N.nc"`
     for file in $allFiles ; do
         fname=`filename $file`
 
@@ -30,13 +31,22 @@ for dim in $dims ; do
         month=$((${fname:4:2} - 1))
         year=${fname:0:4}
         
-        if [ ! -e ${outDir}/${dim}/${fname::-21}.nc ]
+        if [ ! -e ${outDir}/${dim}/${fname:0:6}.nc ]
         then
             # leapYear check
             date -d $year-02-29 &>/dev/null && monthDays=$daysleap || monthDays=$days
-            thisMonth=`find ${outdir} -type f -name "${fname::-21}*_${dim}_ps5km60N.nc"`
-            if [[ $(( monthDays[${month}] * 8 )) == ${#thisMonth[@]} ]] ; then
-                ncra $thisMonth ${outDir}/${dim}/${fname:0:6}.nc
+            thisMonth=`find ${outdir} -type l,f -name "${fname:0:6}*${dim}_ps5km60N.nc"`
+            count=`du -ch $thisMonth | tail -1 | cut -f 1`
+            if [[ $(( ${monthDays[$month]} * 8 )) == $((${count} + 1)) ]]
+            then
+                ncra -o ${outDir}/${dim}/${fname:0:6}.nc $thisMonth
+
+                # fix timestamp
+                ncrename -v time,time_old ${outDir}/${dim}/${fname:0:6}.nc 2>> $logfile
+                ncks -A -v time ${thisMonth[-1]} ${outDir}/${dim}/${fname:0:6}.nc 2>> $logfile
+                ncks -O -x -v 'time_old' ${outDir}/${dim}/${fname:0:6}.nc ${outDir}/${dim}/${fname:0:6}.nc 2>> $logfile
+            else
+                echo "Files missing for ${fname:0:6}"
             fi
         fi
     done
